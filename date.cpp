d0bda6df7da321a0b8edc206b4221b851f17810d
Cagrihan Akca <cagrihanakca@gmail.com>
2026-02-23 14:05:34 +0300

    define the cstring ctor

--- date.cpp
+++ date.cpp
@@ -1,6 +1,10 @@
 #include "date.h"
 
+#include <cstdlib>
+
 namespace pro
 {
     Date::Date(int day, int mon, int year) : m_day{ day }, m_mon{ mon }, m_year{ year } {}
+
+    Date::Date(const char *p) : m_day{ std::atoi(p) }, m_mon{ std::atoi(p + 3) }, m_year{ std::atoi(p + 6) } {}
 }
