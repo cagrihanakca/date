b9bd1bb0d6a3656724967755da6e0146e23152f0
Cagrihan Akca <cagrihanakca@gmail.com>
2026-02-24 15:19:45 +0300

    define global operator>= function that compares two dates

--- date.cpp
+++ date.cpp
@@ -141,6 +141,11 @@ namespace pro
         return rhs < lhs;
     }
 
+    bool operator>=(const Date &lhs, const Date &rhs)
+    {
+        return !(lhs < rhs);
+    }
+
     bool Date::IsLeap(int year)
     {
         return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
