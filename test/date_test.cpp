5dd1aa47805d660156fdf84f96a791deb61b1411
Cagrihan Akca <cagrihanakca@gmail.com>
2026-03-19 12:43:23 +0300

    change the name of the test variable

--- test/date_test.cpp
+++ test/date_test.cpp
@@ -4,19 +4,19 @@
 using namespace pro;
 
 TEST(ConstructorTest, DefaultCtor) {
-    Date d;
-    EXPECT_EQ(d.MonthDay(), 1);
-    EXPECT_EQ(d.Month(), 1);
-    EXPECT_EQ(d.Year(), Date::baseYear);
+    Date testDate;
+    EXPECT_EQ(testDate.MonthDay(), 1);
+    EXPECT_EQ(testDate.Month(), 1);
+    EXPECT_EQ(testDate.Year(), Date::baseYear);
     EXPECT_NO_THROW(Date{});
 }
 
 TEST(ConstructorTest, DayMonYearCtor) {
-    Date d{ 16, 12, 2024 };
-    EXPECT_EQ(d.MonthDay(), 16);
-    EXPECT_EQ(d.Month(), 12);
-    EXPECT_EQ(d.Year(), 2024);
-    EXPECT_NO_THROW(Date(16, 12, 2024));
+    Date testDate{ 16, 12, 2024 };
+    EXPECT_EQ(testDate.MonthDay(), 16);
+    EXPECT_EQ(testDate.Month(), 12);
+    EXPECT_EQ(testDate.Year(), 2024);
+    EXPECT_NO_THROW(Date(12, 12, 2024));
     EXPECT_THROW(Date(38, 12, 2024), std::invalid_argument);
     EXPECT_THROW(Date(12, 65, 2024), std::invalid_argument);
     EXPECT_THROW(Date(12, 12, 1474), std::invalid_argument);
