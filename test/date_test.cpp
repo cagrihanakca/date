7cbda2ae208fded3a876c412b1da82b98f1cf12b
Cagrihan Akca <cagrihanakca@gmail.com>
2026-03-19 12:44:54 +0300

    change the coding style of the test macros

--- test/date_test.cpp
+++ test/date_test.cpp
@@ -3,20 +3,27 @@
 
 using namespace pro;
 
-TEST(ConstructorTest, DefaultCtor) {
+TEST(ConstructorTest, DefaultCtor)
+{
     Date testDate;
+
     EXPECT_EQ(testDate.MonthDay(), 1);
     EXPECT_EQ(testDate.Month(), 1);
     EXPECT_EQ(testDate.Year(), Date::baseYear);
+
     EXPECT_NO_THROW(Date{});
 }
 
-TEST(ConstructorTest, DayMonYearCtor) {
+TEST(ConstructorTest, DayMonYearCtor)
+{
     Date testDate{ 12, 12, 2024 };
+
     EXPECT_EQ(testDate.MonthDay(), 12);
     EXPECT_EQ(testDate.Month(), 12);
     EXPECT_EQ(testDate.Year(), 2024);
+
     EXPECT_NO_THROW(Date(12, 12, 2024));
+
     EXPECT_THROW(Date(38, 12, 2024), std::invalid_argument);
     EXPECT_THROW(Date(12, 65, 2024), std::invalid_argument);
     EXPECT_THROW(Date(12, 12, 1474), std::invalid_argument);
