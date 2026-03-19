ab02ae85d62966cd3d02abdba79a8e9fcc5236be
Cagrihan Akca <cagrihanakca@gmail.com>
2026-03-19 12:51:09 +0300

    add a validity test for cstring ctor

--- test/date_test.cpp
+++ test/date_test.cpp
@@ -28,3 +28,23 @@ TEST(ConstructorTest, DayMonYearCtor)
     EXPECT_THROW(Date(12, 65, 2024), std::invalid_argument);
     EXPECT_THROW(Date(12, 12, 1474), std::invalid_argument);
 }
+
+TEST(ConstructorTest, CStringCtor)
+{
+    Date testDate{ "12/12/2024" };
+
+    EXPECT_EQ(testDate.MonthDay(), 12);
+    EXPECT_EQ(testDate.Month(), 12);
+    EXPECT_EQ(testDate.Year(), 2024);
+
+    EXPECT_NO_THROW(Date("12/12/2024"));
+
+    EXPECT_THROW(Date{ "38/12/2024" }, std::invalid_argument);
+    EXPECT_THROW(Date{ "12/65/2024" }, std::invalid_argument);
+    EXPECT_THROW(Date{ "12/12/1474" }, std::invalid_argument);
+    EXPECT_THROW(Date{ "12 12 2024" }, std::invalid_argument);
+    EXPECT_THROW(Date{ "12.12.2024" }, std::invalid_argument);
+    EXPECT_THROW(Date{ "aaaa12/12/2024" }, std::invalid_argument);
+    EXPECT_THROW(Date{ "12/bbbb12/2024" }, std::invalid_argument);
+    EXPECT_THROW(Date{ "12/12/ccccc2024" }, std::invalid_argument);
+}
