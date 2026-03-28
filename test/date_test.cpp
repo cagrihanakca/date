6c706501c56dbf8124c4444d478650225ce3e527
Cagrihan Akca <cagrihanakca@gmail.com>
2026-03-28 13:53:36 +0300

    add default ctor test

--- test/date_test.cpp
+++ test/date_test.cpp
@@ -27,3 +27,13 @@ TEST(InvalidDateTest, What)
     Date::InvalidDate ex{ MONTH, "invalid month" };
     EXPECT_STREQ(ex.what(), "invalid month");
 }
+
+TEST(CtorTest, DefaultCtor)
+{
+    ASSERT_NO_THROW(Date{});
+
+    Date d;
+    EXPECT_EQ(d.Day(), 1);
+    EXPECT_EQ(d.Month(), 1);
+    EXPECT_EQ(d.Year(), 1900);
+}
