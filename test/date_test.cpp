6a2360dafad460a9bcab7733faf970ac68d5835a
Cagrihan Akca <cagrihanakca@gmail.com>
2026-03-28 14:10:51 +0300

    add cstring ctor test

--- test/date_test.cpp
+++ test/date_test.cpp
@@ -55,3 +55,29 @@ TEST(CtorTest, DayMonYearCtor)
     EXPECT_THROW(Date(30, 2, 2024), Date::InvalidDate);
     EXPECT_THROW(Date(29, 2, 2023), Date::InvalidDate);
 }
+
+TEST(CtorTest, CStringCtor)
+{
+    ASSERT_NO_THROW(Date{ "12/12/2024" });
+
+    Date d{ "12/12/2024" };
+    EXPECT_EQ(d.Day(), 12);
+    EXPECT_EQ(d.Month(), 12);
+    EXPECT_EQ(d.Year(), 2024);
+
+    EXPECT_THROW(Date{ "0/12/2024" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "32/12/2024" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "12/0/2024" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "12/12/1899" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "31/4/2024" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "30/2/2024" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "29/2/2023" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "2/12/2024" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "12/2/2024" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "12/12/20242" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "12 12 2024" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "12.12.2024" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "aaaa12/12/2024" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "12/bbbb12/2024" }, Date::InvalidDate);
+    EXPECT_THROW(Date{ "12/12/ccccc2024" }, Date::InvalidDate);
+}
