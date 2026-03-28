65cd70f8e85d3c7e2a3aff4fc93ed676e833fdca
Cagrihan Akca <cagrihanakca@gmail.com>
2026-03-28 11:58:03 +0300

    add nested type test

---
+++ test/date_test.cpp
@@ -0,0 +1,29 @@
+#include "date.h"
+#include "gtest/gtest.h"
+
+using namespace cgr;
+
+TEST(InvalidDateTest, Ctor)
+{
+    EXPECT_NO_THROW(Date::InvalidDate(Date::InvalidDate::Reason::DAY, "invalid day"));
+}
+
+TEST(InvalidDateTest, GetReason)
+{
+    using Date::InvalidDate::Reason::DAY;
+
+    ASSERT_NO_THROW(Date::InvalidDate(DAY, "invalid day"));
+
+    Date::InvalidDate ex{ DAY, "invalid day" };
+    EXPECT_EQ(ex.GetReason(), DAY);
+}
+
+TEST(InvalidDateTest, What)
+{
+    using Date::InvalidDate::Reason::MONTH;
+
+    ASSERT_NO_THROW(Date::InvalidDate(MONTH, "invalid month"));
+
+    Date::InvalidDate ex{ MONTH, "invalid month" };
+    EXPECT_STREQ(ex.what(), "invalid month");
+}
