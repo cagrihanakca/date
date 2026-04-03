24d17313ba9c41bcf4d056783b699c970656ef55
Cagrihan Akca <cagrihanakca@gmail.com>
2026-04-03 20:11:22 +0300

    update example of the project

--- example/main.cpp
+++ example/main.cpp
@@ -3,30 +3,22 @@
 
 int main()
 {
-    // construction
+    // construction & getters
     cgr::Date d1{ "12/12/2024" };
-    cgr::Date d2{ 7, 10, 2018 };
-
-    // getters
-    std::cout << "date1 : " << d1 << '\n';
+    std::cout << "date  : " << d1 << '\n';
     std::cout << "day   : " << d1.Day() << '\n';
     std::cout << "month : " << d1.Month() << '\n';
     std::cout << "year  : " << d1.Year() << "\n\n";
-
-    std::cout << "date2 : " << d2 << '\n';
-    std::cout << "day   : " << d2.Day() << '\n';
-    std::cout << "month : " << d2.Month() << '\n';
-    std::cout << "year  : " << d2.Year() << "\n\n";
 
     // setter chaining
     d1 = cgr::Date{ "11/11/2011" };
     std::cout << "date before chaining : " << d1 << '\n';
     d1.Day(25).Month(3).Year(2026);
-    std::cout << "date after chaining  : "<< d1 << "\n\n";
+    std::cout << "date after chaining  : " << d1 << "\n\n";
 
     // arithmetic
     d1 = cgr::Date{ "11/11/2011" };
-    d2 = cgr::Date{ "10/10/2010" };
+    cgr::Date d2{ 7, 10, 2018 };
     std::cout << "date1         : " << d1 << '\n';
     std::cout << "date2         : " << d2 << '\n';
     std::cout << "date1 - date2 : " << d1 - d2 << " days\n";
@@ -44,8 +36,8 @@ int main()
     std::cout << "date1 == date2 : " << (d1 == d2) << "\n\n";
 
     // static functions
-    std::cout << "today        : " << cgr::Date::Today() << "\n\n";
-    std::cout << "random date  : " << cgr::Date::RandomDate() << "\n\n";
+    std::cout << "today        : " << cgr::Date::Today() << '\n';
+    std::cout << "random date  : " << cgr::Date::RandomDate() << '\n';
     std::cout << "is 1900 leap : " << cgr::Date::IsLeap(1900) << '\n';
     std::cout << "is 2000 leap : " << cgr::Date::IsLeap(2000) << "\n\n";
 
